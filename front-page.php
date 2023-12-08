56368c054b97f34110178f0197e3197ea4e474c1
mysteriou <elio007@hotmail.fr>
2023-12-08 09:43:07 +0100

    ajout file style.css dans ./header.php

--- front-page.php
+++ front-page.php
@@ -8,9 +8,9 @@
             the_post();
         ?>
             <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-                <header class="entry-header">
+                <div class="entry-header">
                     <?php the_title('<h2 class="entry-title"><a href="' . esc_url(get_permalink()) . '" rel="bookmark">', '</a></h2>'); ?>
-                </header>
+             </div>
 
                 <div class="entry-content">
                     <?php the_excerpt(); ?>
