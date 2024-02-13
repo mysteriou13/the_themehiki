5e2bd6b0f69262d7d25f382e6295be7e10d1dab2
mysteriou <elio007@hotmail.fr>
2024-02-13 19:33:03 +0100

    ajout affichage de la page dans page.php

--- page.php
+++ page.php
@@ -1,12 +1,6 @@
-<?php get_header(); ?>
-
-
-
 
 <?php
-get_header(); // Inclure l'en-tête standard de WordPress
-
-
+ get_header();
 
 // Récupérer l'URL actuelle
 $current_url = $_SERVER['REQUEST_URI'];
@@ -19,6 +13,8 @@ $cleaned_path = str_replace('/', '', $url_parts['path']);
 
 $tab = ['login','registration'];
 
+$path = false;
+
 for($a = 0; $a < count($tab); $a++){
 
 if ($cleaned_path ===  $tab[$a] && isset($url_parts['path'])) {
@@ -27,19 +23,49 @@ if ($cleaned_path ===  $tab[$a] && isset($url_parts['path'])) {
     
     include_once(get_template_directory()."/template/".$tab[$a].".php");
 
-}
+    $path = true;
+    break;
 
 }
 
+}
+
+if(!$path){
 ?>
+
+<div class="box_front_page text-light">
+<div id="primary" class="content-area">
+
+    <?php while (have_posts()) : the_post(); ?>
+      <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+          <h1 class="entry-title"><?php the_title(); ?></h1>
+
+        <div class="entry-content">
+          <?php the_content(); ?>
+        </div>
+      </article>
+    <?php endwhile; ?>
+
+</div>
+        </div>
+
+
+<?php
+        
+        ?>
 
     </main>
 </div>
 
 
-
-<?php 
-
-get_footer();
+<?php
+}
 
 ?>
+
+</div>   
+
+<?php
+wp_footer();
+get_footer(); ?>
